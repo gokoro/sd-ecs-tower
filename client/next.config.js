75f6b23cdc48cf17efbbfd05d476ecf0426df7f0
gokoro <lincroe@gmail.com>
2023-11-09 21:03:35 +0900

    Use standalone again

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,7 @@ const withVanillaExtract = createVanillaExtractPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/tower',
+  output: 'standalone',
 }
 
 const wrappedNextConfig = withVanillaExtract(nextConfig)
