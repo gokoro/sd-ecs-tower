6e58838347c2a8d59955026acbc0d51521e86e5e
gokoro <lincroe@gmail.com>
2023-12-03 14:54:58 +0900

    test

--- client/src/utils/server-api.ts
+++ client/src/utils/server-api.ts
@@ -1,5 +1,6 @@
 import ky from 'ky'
 
 const towerEndpoint = process.env.SERVER_SIDE_TOWER_ENDPOINT
+console.log('SERVER_SIDE_TOWER_ENDPOINT:', towerEndpoint)
 
 export const serverTowerApi = ky.create({ prefixUrl: towerEndpoint, fetch })
